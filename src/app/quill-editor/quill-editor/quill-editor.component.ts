b4de813f4559024077bdc8c3185ea6ef1e3da985
komura <affirman03139@outlook.jp>
2020-08-28 01:43:53 +0900

    fix(quill): add nino fix, delete image-resize

--- src/app/quill-editor/quill-editor/quill-editor.component.ts
+++ src/app/quill-editor/quill-editor/quill-editor.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
 import 'quill-mention';
 import 'quill-emoji';
-
-import Quill from 'quill';
-import ImageResize from 'quill-image-resize-module';
-Quill.register('modules/imageResize', ImageResize);
 
 @Component({
   selector: 'app-quill-editor',
@@ -15,68 +10,48 @@ Quill.register('modules/imageResize', ImageResize);
   styleUrls: ['./quill-editor.component.scss']
 })
 export class QuillEditorComponent implements OnInit {
+  @ViewChild('imageInput') private imageInput: ElementRef<HTMLElement>;
   form = this.fb.group({
     editorContent: [''],
   });
   get editorContentControl() {
     return this.form.get('editorContent') as FormControl;
   }
-  content: string;
 
-  quillEditorRef: any;
-  quillFileInput = document.createElement('input');
-
+  editorInstance: any;
   atValues = [
     { id: 1, value: 'hanako', link: 'https://google.com' },
     { id: 2, value: 'tarou' },
     { id: 3, value: 'satoshi' },
-    { id: 4, value: 'keiko' }
   ];
   hashValues = [
     { id: 5, value: 'cat' },
     { id: 6, value: 'dog' },
     { id: 7, value: 'music' }
   ];
-
   modules = {
     toolbar: {
       container: [
-        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
         ['blockquote', 'code-block'],
-
-        [{ header: 1 }, { header: 2 }],               // custom button values
+        [{ header: 1 }, { header: 2 }], // custom button values
         [{ list: 'ordered' }, { list: 'bullet' }],
-        [{ script: 'sub' }, { script: 'super' }],      // superscript/subscript
-        [{ indent: '-1' }, { indent: '+1' }],          // outdent/indent
-        [{ direction: 'rtl' }],                         // text direction
-
-        [{ size: ['small', false, 'large', 'huge'] }],  // custom dropdown
+        [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+        [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+        [{ direction: 'rtl' }], // text direction
+        [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
         [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-        [{ color: [] }, { background: [] }],          // dropdown with defaults from theme
+        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
         [{ font: [] }],
         [{ align: [] }],
-
-        ['clean'],                                         // remove formatting button
-
-        ['link', 'image', 'video'],                         // link and image, video
+        ['clean'], // remove formatting button
+        ['link', 'image', 'video'], // link and image, video
         ['emoji']
       ],
       handlers: {
-        // handlerはイベント自体を置き換える
-        image: (isEmit: boolean) => {
-          if (isEmit) {
-            this.quillFileInput.setAttribute('type', 'file');
-            this.quillFileInput.setAttribute('accept', 'image/*');
-            this.quillFileInput.addEventListener('change', (event) => {
-              this.quillFileUpload(event);
-            });
-            this.quillFileInput.click();
-          }
-        }
+        image: this.selectImage.bind(this),
       }
     },
-    imageResize: true,
     'emoji-shortname': true,
     'emoji-textarea': true,
     'emoji-toolbar': true,
@@ -102,101 +77,34 @@ export class QuillEditorComponent implements OnInit {
           renderList(matches, searchTerm);
         }
       },
-    },
-    keyboard: {
-      bindings: {
-        shiftEnter: {
-          key: 13,
-          shiftKey: true,
-          handler: (range, context) => {
-            // Handle shift+enter
-            console.log('shift+enter');
-          }
-        },
-        enter: {
-          key: 13,
-          handler: (range, context) => {
-            console.log('enter');
-            return true;
-          }
-        }
-      }
     }
   };
-  blured = false;
-  focused = false;
 
   constructor(
     private fb: FormBuilder,
-    private ngZone: NgZone,
-    private snackBar: MatSnackBar
   ) { }
 
-  created(quill) {
-    this.quillEditorRef = quill;
-    console.log('editor-created', quill);
+  created(editorInstance) {
+    this.editorInstance = editorInstance;
+    console.log('editor-created', editorInstance);
   }
 
   changedEditor(event: EditorChangeContent | EditorChangeSelection) {
     console.log('editor-change', event);
   }
 
-  focus($event) {
-    console.log('focus', $event);
-    this.focused = true;
-    this.blured = false;
+  selectImage() {
+    this.imageInput.nativeElement.click();
   }
 
-  blur($event) {
-    console.log('blur', $event);
-    this.focused = false;
-    this.blured = true;
-  }
-
-  addBindingCreated(quill) {
-    quill.keyboard.addBinding({
-      key: 'b'
-    }, (range, context) => {
-      console.log('KEYBINDING B', range, context);
-    });
-
-    quill.keyboard.addBinding({
-      key: 'B',
-      shiftKey: true
-    }, (range, context) => {
-      console.log('KEYBINDING SHIFT + B', range, context);
-    });
-  }
-
-  quillFileUpload(event) {
-    if (event.target.files.length) {
-      const file = event.target.files[0];
-      console.log(file);
-      const fileSizeLimit = 3000000;
-      if (file.size < fileSizeLimit) {
-        // 画像をアップロード
-        // const downloadURLPromise = this.imageService.uploadImage(
-        //   userId,
-        //   file
-        // );
-
-        // insertEmbedイベントを使って画像を挿入
-        // downloadURLPromise.then((downloadURL) => {
-        //   this.quillEditorRef.insertEmbed(0, 'image', downloadURL, 'user');
-        // });
-
-        return;
-      } else {
-        this.ngZone.run(() => {
-          const msg = '３メガバイト未満の画像を利用してください';
-          this.snackBar.open(msg, '閉じる');
-        });
-        return;
-      }
-    }
+  async uploadAndInsertImage({ target }: { target: HTMLInputElement }) {
+    const image = target.files[0];
+    console.log(image);
+    const selection = this.editorInstance.getSelection();
+    console.log(selection);
+    // this.editorInstance.insertEmbed(selection.index, 'image', imageURL);
   }
 
   ngOnInit(): void {
   }
-
 }
